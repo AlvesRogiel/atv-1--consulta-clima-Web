a56c529b3f12cf70b8ae0352bc7dd10024c9d756
AlvesRogiel <rogielf@hotmail.com>
2026-09-11 23:49:31 -0300

    versao melhorada

--- js/index.js
+++ js/index.js
@@ -10,25 +10,35 @@ campoCidade.addEventListener("keydown", function (evento) {
 });
 
 async function buscarCidade() {
-    let nome = campoCidade.value;
-    elementoMensagem.textContent = "Buscando...";
-    let resposta = await fetch(`https://brasilapi.com.br/api/cptec/v1/cidade/${nome}`)
-    console.log(resposta);
-    let dados = await resposta.json();
-    if (resposta.ok) {
-        for (let i = 0; i < dados.length; i++) {
-            let elementoCidade = document.createElement("p");
-            elementoCidade.textContent = `${dados[i].nome} - ${dados[i].estado}`;
-            elementoCidade.classList.add("cidade")
-            elementoCidade.addEventListener("click", function () {
-                buscarPrevisao(dados[i].id)
-            });
-            elementoCidades.appendChild(elementoCidade);
-        }
-        elementoMensagem.textContent = ""
+    let nome = campoCidade.value.trim();
+    if (nome == "") {
+        elementoMensagem.textContent = "Digite o nome de uma cidade.";
+        elementoCidades.innerHTML = "";
+        elementoPrevisao.innerHTML = "";
     } else {
-        elementoMensagem.textContent = dados.message;
+        elementoMensagem.textContent = "Buscando...";
+        let resposta = await fetch(`https://brasilapi.com.br/api/cptec/v1/cidade/${nome}`)
+        //https://brasilapi.com.br/api/cptec/v1/cidade/
+        console.log(resposta);
+        let dados = await resposta.json();
+        if (resposta.ok) {
+            for (let i = 0; i < dados.length; i++) {
+                let elementoCidade = document.createElement("button");
+                elementoCidade.type = "type";
+                elementoCidade.textContent = `${dados[i].nome} - ${dados[i].estado}`;
+                elementoCidade.classList.add("cidade")
+                elementoCidade.addEventListener("click", function () {
+                    buscarPrevisao(dados[i].id)
+                });
+                elementoCidades.appendChild(elementoCidade);
+            }
+            elementoMensagem.textContent = `$(dados.length) cidade(s) encontrada(s)`
+        } else {
+            elementoMensagem.textContent = "Nenhuma cidade localizada";
+        }
     }
+
+
 }
 
 async function buscarPrevisao(id) {
@@ -36,20 +46,25 @@ async function buscarPrevisao(id) {
     let resposta = await fetch(`https://brasilapi.com.br/api/cptec/v1/clima/previsao/${id}`)
     let dados = await resposta.json();
     if (resposta.ok) {
+        let dias = `<artcle class"dia">
+    <p class="data">${formatarData(dados.clima[0].data)}</p>
+    <p>${dados.clima[0].condicao_desc}</p>
+    <div class= "temperaturas">
+    <apan><strong>${dados.clima[0].min} °C</strong>Mínima</span>
+    <apan><strong>${dados.clima[0].max} °C</strong>M</spaáximan>
+    </div>
+    <p>Índice UV: ${dados.clima[0].indice_uv}</p>
+    </article>
+        `;
         elementoPrevisao.innerHTML = `
     <h2>${dados.cidade} - ${dados.estado}</h2>
-    <div class"dia">
-    <p>Data: ${formatarData(dados.clima[0].data)}</p>
-    <p>Condição: ${dados.clima[0].condicao_desc}</p>
-    <p>Temperatura mínima: ${dados.clima[0].min} °C</p>
-    <p>Temperatura máxima: ${dados.clima[0].max} °C</p>
-    <p>Índice UV: ${dados.clima[0].indice_uv}</p>
-    </div>
-    `
+    <div class"dias">${dias}</div>
+    `;
     } else {
         elementoPrevisao.textContent = dados.message;
     }
-    console.log(dados);
+    elementoCidades.innerHTML = "";
+    elementoMensagem.textContent = "";
 }
 
 function formatarData(data) {
